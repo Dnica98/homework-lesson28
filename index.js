7ef8b8a85999781405a53a9f539ae812976ce4dc
Dnica98 <daniela.nica98@gmail.com>
2024-11-08 17:17:12 +0200

    1

---
+++ index.js
@@ -0,0 +1,42 @@
+const promiseTest = document.getElementById('promiseTest')
+
+const userID = 1
+
+function displayData(data) {
+    const dataElement = document.createElement('ol')
+    dataElement.innerText= JSON.stringify(data, null)
+    promiseTest.appendChild(dataElement)
+}
+
+fetch(`https://jsonplaceholder.typicode.com/users/${userID}?`, {
+    method:'GET'
+})
+
+.then((res)=> res.json())
+.then((data)=> displayData(data))
+.catch((err)=>console.log(err))
+
+
+fetch('https://jsonplaceholder.typicode.com/users?_limit=5',{method: 'GET'})
+
+.then((res) =>res.json())
+.then((data)=> {
+    data.forEach(displayData)})
+.catch((err)=>console.log(err))
+
+fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'POST',
+    body: JSON.stringify({
+        name: 'Leanne Graham',
+        email:'Sincere@april.biz',
+        username: 'Bret'
+    }),
+    headers:{
+        'Content-type': 'application/json; charset-UTF-8',
+        service: 'Sales service'
+    }
+})
+
+.then((res) => res.json())
+.then((data)=> displayData(data))
+.catch((err)=>console.log(err))
